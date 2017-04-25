1fb6f758a49015ce2faaaa23f5ce7ea85348510b
Tyler Shaw <tylershaw80@yahoo.com>
2017-04-25 00:08:28 -0400

    Implement Google Analytics tracking code.

--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -6,7 +6,16 @@
     <meta name="description" content="Interactive world map for 7 Days to Die. Up-to-date with markers showing traders, caves, bomb shelters and more!">
     <title>7 Days to Die Map - 7daysmap.com</title>
     <link rel="stylesheet" href="{{ elixir ('css/map.css') }}">
-    {{-- Analytics Code Here --}}
+    @if(config('app.env') == 'production')
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+            ga('create', 'UA-98016531-1', 'auto');
+            ga('send', 'pageview');
+        </script>
+    @endif
 </head>
 <body>
     <div id="map-container"></div>
